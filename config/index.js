dab78aba0202b9066c2640019c3be803886fb907
AraonChen <66001478+chenzhihao-11@users.noreply.github.com>
2022-09-13 21:03:27 +0800

    111
    111

--- config/index.js
+++ config/index.js
@@ -33,30 +33,36 @@ export const config = {
       // 所在城市
       city: "上海",
       // 新历生日, 仅用作获取星座运势, 格式必须
-      horoscopeDate: '12-27',
+      horoscopeDate: '06-07',
       // 获取什么时候的星座运势，可选：['今日', '明日', '本周', '本月', '今年'], 留空则随机
       horoscopeDateType: '今日',
       // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
       openUrl: "https://wangxinleo.cn",
       // 专属节日提醒，如果你在这里填写了节日提醒，就不会执行FESTIVALS的提醒了, 和FESTIVALS的配置方法相同，可以往下查看，我这里就不重复写了
-      festivals: [
-        // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
-        {"type": "*生日", "name": "老婆", "year": "1996", "date": "09-09"},
-        {"type": "节日", "name": "结婚纪念日", "year": "2020", "date": "09-03"},
-        // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
-        {"type": "生日", "name": "李四", "year": "1996", "date": "09-31"},
-        {"type": "节日", "name": "被搭讪纪念日", "year": "2021", "date": "09-01"},
+//       festivals: [
+//         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
+//         {"type": "*生日", "name": "老婆", "year": "1996", "date": "09-09"},
+//         {"type": "节日", "name": "结婚纪念日", "year": "2020", "date": "09-03"},
+//         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
+//         {"type": "生日", "name": "李四", "year": "1996", "date": "09-31"},
+//         {"type": "节日", "name": "被搭讪纪念日", "year": "2021", "date": "09-01"},
+//       ],
+       festivals: [
+          {"type": "节日", "name": "国庆节", "year": "2022", "date": "10-01"},
+          {"type": "节日", "name": "元旦", "year": "2023", "date": "01-01"},
+          {"type": "节日", "name": "春节", "year": "2023", "date": "01-22"},
+
       ],
-      // 专属纪念日/倒数日，如果你在这里填写了纪念日/倒数日，就不会计算CUSTOMIZED_DATE_LIST的日子了, 和CUSTOMIZED_DATE_LIST的配置方法相同，可以往下查看，我这里就不重复写了
-      customizedDateList: [
-        // 在一起的日子
-        {"keyword": "love_day", date: "2022-09-08"},
-        // 结婚纪念日
-        {"keyword": "marry_day", date: "2022-09-09"},
-        // 退伍日
-        {"keyword": "ex_day", date: "2022-09-10"},
-      ]
-    },
+//       // 专属纪念日/倒数日，如果你在这里填写了纪念日/倒数日，就不会计算CUSTOMIZED_DATE_LIST的日子了, 和CUSTOMIZED_DATE_LIST的配置方法相同，可以往下查看，我这里就不重复写了
+//       customizedDateList: [
+//         // 在一起的日子
+//         {"keyword": "love_day", date: "2022-09-08"},
+//         // 结婚纪念日
+//         {"keyword": "marry_day", date: "2022-09-09"},
+//         // 退伍日
+//         {"keyword": "ex_day", date: "2022-09-10"},
+//       ]
+//     },
 //     {
 //       name: "老婆1",
 //       id: "",
@@ -148,11 +154,12 @@ export const config = {
    */
   FESTIVALS: [
     // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
-    {"type": "*生日", "name": "老婆", "year": "1996", "date": "09-09", isShowAge: true},
-    {"type": "节日", "name": "结婚纪念日", "year": "2020", "date": "09-03"},
+//     {"type": "*生日", "name": "老婆", "year": "1996", "date": "09-09", isShowAge: true},
+    {"type": "节日", "name": "国庆节", "year": "2022", "date": "10-01"},
     // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
-    {"type": "生日", "name": "李四", "year": "1996", "date": "09-31", isShowAge: true},
-    {"type": "节日", "name": "被搭讪纪念日", "year": "2021", "date": "09-01"},
+//     {"type": "生日", "name": "李四", "year": "1996", "date": "09-31", isShowAge: true},
+    {"type": "节日", "name": "元旦", "year": "2023", "date": "01-01"},
+    {"type": "节日", "name": "春节", "year": "2023", "date": "01-22"},
     // 你可以不断按格式往下增加
     // ...
   ],
@@ -170,16 +177,16 @@ export const config = {
     * keyword是指暴露给测试号的模板字段，填什么就暴露什么, 请注意不要和README的出参表中的字段重复。
     * 比如：keyword: "love_date" ，在测试号中就是 {{ love_date.DATA }}
     * */
-  CUSTOMIZED_DATE_LIST: [
-    // 在一起的日子
-    {"keyword": "love_day", date: "2022-09-08"},
-    // 结婚纪念日
-    {"keyword": "marry_day", date: "2022-09-09"},
-    // 退伍日
-    {"keyword": "ex_day", date: "2022-09-10"},
-    // 你可以不断按格式往下增加
-    // ...
-  ],
+//   CUSTOMIZED_DATE_LIST: [
+//     // 在一起的日子
+//     {"keyword": "love_day", date: "2022-09-08"},
+//     // 结婚纪念日
+//     {"keyword": "marry_day", date: "2022-09-09"},
+//     // 退伍日
+//     {"keyword": "ex_day", date: "2022-09-10"},
+//     // 你可以不断按格式往下增加
+//     // ...
+//   ],
 
   /** 插槽 */
 
